c4935c6a3335f8c1cfe73ca568e47f2caf3aae1d
adrian hartanto <71419039+AdrianHartanto107@users.noreply.github.com>
2020-10-03 08:34:25 +0700

    Add files via upload

---
+++ phpmvc/app/models/User_model.php
@@ -0,0 +1,10 @@
+<?php 
+
+class User_model {
+	private $nama = 'Candy';
+
+	public function getUser()
+	{
+		return $this->nama;
+	}
+}
---
+++ phpmvc/app/views/about/index.php
@@ -0,0 +1,7 @@
+<div class="container">
+
+	<h1 class="mt-4">About Me</h1>
+	<img src="<?= BASEURL; ?>/img/wr.png" alt="Wind ranger" width="400" height= "300" class="rounded-circle shadow">
+	<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya seorang <?= $data['pekerjaan']; ?>.</p>
+
+	</div>
